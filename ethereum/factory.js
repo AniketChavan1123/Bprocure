431c66c46cf4a55f81b6ab69eb34bdfc460e4bbc
Aniket Chavan <87638342+AniketChavan1123@users.noreply.github.com>
2022-04-11 18:00:07 +0530

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import ContractFactory from "./build/ContractFactory.json";
 
 const instance = new web3.eth.Contract(
   ContractFactory.abi,
-  "0x5901dA46585B7C670A25342B1323c5dFb190CF0B"
+  "0x9Df4F6882FDF78F25a5c1d1633EE4fEF538eef4b"
 );
 export default instance;
 // 0xdA5B5AfA46C2A04cabc2A132D3d35F06F3918b9c
