bb7507f52c1778aeca972165eec359c96171428c
AniketChavan1123 <aniketchavan_20816@aitpune.edu.in>
2022-04-12 17:19:45 +0530

    factory address changed

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import ContractFactory from "./build/ContractFactory.json";
 
 const instance = new web3.eth.Contract(
   ContractFactory.abi,
-  "0x9Df4F6882FDF78F25a5c1d1633EE4fEF538eef4b"
+  "0x3af6aC8a99D39CDec5f9539630e310C0984D7705"
 );
 export default instance;
 // 0xdA5B5AfA46C2A04cabc2A132D3d35F06F3918b9c
